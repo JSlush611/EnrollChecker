db423623a9f23dc19ea310d5311dad039a7aabec
JSlush611 <jschlues611@gmail.com>
2023-09-13 17:15:25 -0500

    Refactored poller.js file

--- poller.js
+++ poller.js
@@ -3,21 +3,34 @@ import axios from "axios"
 
 dotenv.config({ path: './server/.env' });
 
-const POLLING_INTERVAL_MS = 30000;
+const POLLING_INTERVAL_MS = 10000;
  
-function requestCoursePoll() {
-    setInterval(async () => {
-        try {
-            const response = await axios.get('http://localhost:3001/admin/pollcourses', {
-                headers: {
-                    'Admin-Key': process.env.ADMIN_KEY,
-                },
-            });
-            console.log('Successful poll: :', response.data);
-        } catch (error) {
-            console.error('Error polling: :', error.message);
-        }
-    }, POLLING_INTERVAL_MS);
+async function printTimeStamp(courseAvailabilityData) {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(time, '- Successful poll:', courseAvailabilityData);
 }
 
-requestCoursePoll();
+async function getCoursesAvailability() {
+    const response = await axios.get('http://localhost:3001/admin/pollcourses', {
+      headers: {
+        'Admin-Key': process.env.ADMIN_KEY,
+      },
+    });
+    return response.data;
+};
+
+async function requestCoursePoll() {
+    try {
+        const courseAvailabilityData = await getCoursesAvailability();
+        printTimeStamp(courseAvailabilityData);
+    } catch (error) {
+        console.error('Error polling: :', error.message);
+    } 
+};
+
+function startPolling() {
+    setInterval(requestCoursePoll, POLLING_INTERVAL_MS); 
+};
+
+startPolling();
